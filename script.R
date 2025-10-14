1343b576b7fb0163807b4d2424dc2f9f8bfac978
civlima <vitoria2ml@gmail.com>
2025-10-14 16:02:02 -0300

    title lab altered

--- script.R
+++ script.R
@@ -10,7 +10,7 @@ ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
   geom_point(aes(color= species, shape = species)) +
   geom_smooth(method = "lm") +
   labs(
-    title = "Body mass and flipper length",
+    title = "ALTERED - Body mass and flipper length",
     subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
     x = "Flipper length (mm)", y = "Body mass(g)",
     color = "Species", shape = "Species"
